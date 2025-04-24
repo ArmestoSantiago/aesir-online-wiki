fa63bd430364550d1a7eee69e67c682962ef1374
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-04-24 17:30:35 -0300

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,14 +15,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://aesir-online.net',
+  url: 'https://armestosantiago.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/aesir-online-wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Santiago Armesto', // Usually your GitHub org/user name.
+  organizationName: 'armestosantiago', // Usually your GitHub org/user name.
   projectName: 'aesir-online-wiki', // Usually your repo name.
 
   onBrokenLinks: 'throw',
